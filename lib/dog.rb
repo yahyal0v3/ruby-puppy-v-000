b7048a51193249dc0d49ecfa39b50ad2fe235e61
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-10-10 02:37:10 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each do |dog|
+      puts "#{dog.name}"
+    end
+  end
+
+  def self.clear_all
+    @@all.delete_if do |dog|
+      dog == dog
+    end
+  end
+
+end
